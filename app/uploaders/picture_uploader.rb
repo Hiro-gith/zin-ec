c7a6bfbb280802921956cf61c0cd7780429702f4
Hiro <itrails2019@gmail.com>
2020-01-24 05:23:01 +0000

    1/24 ver1

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -16,13 +16,14 @@ class PictureUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  # 画像がなかったときのデフォルト画像の指定
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url(*args) # コメントアウトを外す
+   #   # For Rails 3.1+ asset pipeline compatibility:
+   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+     "default.jpg" # 書き加える
+   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  end # コメントアウトを外す
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
