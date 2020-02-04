36e8561df5a63cdc22abeb522b0e40f290d778ea
Hiro <itrails2019@gmail.com>
2020-02-04 09:09:17 +0000

    2/4 heroku2

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,6 +1,7 @@
 #  CarrierWaveを通してS3を使う
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
