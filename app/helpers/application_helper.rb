72cc0f26dbec21f6a77fb01fd9cb67158eda9e69
Hiro <itrails2019@gmail.com>
2020-02-06 10:44:02 +0000

    2/6 heroku1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper # すべてのビューで読み込まれる
   # ページごとの完全なタイトルを返す
   #デフォルト(引数を指定しないとき)の引数は""
   def full_title(page_title = '')
-    base_title = "Arazin"
+    base_title = "Zin"
     if page_title.empty?
       base_title
     else
