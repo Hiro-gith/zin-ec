6f137d028bd34a8b64ad7c6a5d1b3960778d3d58
Hiro <itrails2019@gmail.com>
2020-03-03 04:18:08 +0000

    3/3 delete ucart

--- db/migrate/20200302091657_destroy_ucart.rb
+++
@@ -1,5 +0,0 @@
-class DestroyUcart < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :ucarts
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200302091657) do
+ActiveRecord::Schema.define(version: 20200301232523) do
 
   create_table "boughts", force: :cascade do |t|
     t.integer "item_id"
